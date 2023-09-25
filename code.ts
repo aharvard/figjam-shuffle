05a7f71d9b25300b095a8c41f4cfb27d798efb6f
Andrew Harvard <aharvard@squareup.com>
2023-09-25 11:57:46 -0400

    fix: use absolute position values to prevent stickies from jumping on update

--- code.ts
+++ code.ts
@@ -1,56 +1,49 @@
-function shuffle(array) {
-  let currentIndex = array.length,
-    randomIndex;
-
-  // While there remain elements to shuffle.
-  while (currentIndex != 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-
-  return array;
-}
-
 const selection = figma.currentPage.selection;
 
 const stickies = selection.filter((node) => node.type === "STICKY");
 
-// get current positions of selected stickies
-const originalPositions: {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-}[] = [];
-stickies.forEach((node) => {
-  originalPositions.push({
-    x: node.x,
-    y: node.y,
-    width: node.width,
-    height: node.height,
-  });
-});
+const stickiesCoordinates = getCoordinates(stickies);
 
-// randomize positions of selected stickies
-shuffle(originalPositions);
+const shuffledStickiesCoordinated = shuffle(stickiesCoordinates);
 
-// update positions of selected stickies
-stickies.forEach((node, index) => {
-  node.x = originalPositions[index].x;
-  node.y = originalPositions[index].y;
-  figma.currentPage.insertChild(
-    Math.floor(stickies.length * Math.random()),
-    node
-  );
-});
-
-// figma.viewport.scrollAndZoomIntoView(stickies);
+updateStickies(stickies, shuffledStickiesCoordinated);
 
 figma.closePlugin();
+
+function updateStickies(selection, coordinates) {
+    selection.forEach((node, index) => {
+        node.x = coordinates[index].x;
+        node.y = coordinates[index].y;
+        figma.currentPage.insertChild(
+            figma.currentPage.children.length - 1,
+            node
+        );
+    });
+}
+
+function getCoordinates(stickies) {
+    return stickies.map((sticky) => ({
+        x: sticky.absoluteBoundingBox.x,
+        y: sticky.absoluteBoundingBox.y,
+    }));
+}
+
+function shuffle(array) {
+    let currentIndex = array.length,
+        randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+        ];
+    }
+
+    return array;
+}
